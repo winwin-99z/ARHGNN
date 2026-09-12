a7a01ca19fad6c3b8f5b0b58d418e50f69200b29
winwin-99z <zwinwin99@gmail.com>
2026-09-12 22:37:47 +0800

    Delete ARHGNN-main directory

--- ARHGNN-main/scripts/run_smoke.ps1
+++
@@ -1,4 +0,0 @@
-Set-StrictMode -Version Latest
-$ErrorActionPreference = "Stop"
-python -m arhgnn.train --config configs/arhgnn.yaml --epochs 2 --bootstrap 10
-
--- ARHGNN-main/scripts/run_tests.ps1
+++
@@ -1,4 +0,0 @@
-Set-StrictMode -Version Latest
-$ErrorActionPreference = "Stop"
-python tests\run_tests.py
-
